2f7c7b1c34f7b6dcbbb53d1487a06163ad3df4dd
Noel <noeldiaz83@gmail.com>
2019-11-04 08:14:23 +0000

    bug if promotion action is CreateLineItems

--- lib/sales.rb
+++ lib/sales.rb
@@ -8,7 +8,11 @@ module Sales
 
 	def get_percent(promotion)
 		if promotion.actions.present?
-			return promotion.actions.first.calculator.preferences.values.first.to_i
+			if promotion.actions.first.type != "Spree::Promotion::Actions::CreateLineItems"
+				return promotion.actions.first.calculator.preferences.values.first.to_i 
+			else
+				return 0
+			end
 		else
 			return 0
 		end
